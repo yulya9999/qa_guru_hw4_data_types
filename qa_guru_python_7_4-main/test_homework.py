85e0d381d4aa6755313ba2d95429d08f2e0dcffb
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-05-11 16:58:30 +0300

    4-е домашнее заданме

---
+++ qa_guru_python_7_4-main/test_homework.py
@@ -0,0 +1,83 @@
+import math
+from random import randint
+
+
+def test_greeting():
+    name = "Анна"
+    age = 25
+    output = f"Привет, {name}! Тебе {age} лет."
+
+    assert output == "Привет, Анна! Тебе 25 лет."
+
+
+def test_rectangle():
+    """
+    Напишите программу, которая берет длину и ширину прямоугольника
+    и считает его периметр и площадь.
+    """
+    a = 10
+    b = 20
+    perimeter = 2*(a+b)
+
+    assert perimeter == 60
+
+    area = a*b
+    assert area == 200
+
+
+def test_circle():
+    """
+    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+    Используйте константу PI
+    """
+    pi = math.pi
+    r = 23
+    area = pi*r**2
+
+    assert area == 1661.9025137490005
+
+    length = 2*pi*r
+
+    assert length == 144.51326206513048
+
+
+def test_random_list():
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
+    """
+
+    l = []
+    for _ in range(10):
+        l.append(randint(1,100))
+    l.sort()
+
+    assert len(l) == 10
+    assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
+
+
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    l = list(set(l))
+
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_dicts():
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Подсказка: используйте встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    d = dict(zip(first, second))
+
+    assert isinstance(d, dict)
+    assert len(d) == 5
+    assert list(d.keys()) == first
+    assert list(d.values()) == second
